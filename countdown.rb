7e31cbf486227f6aebc1df0dfc0111e0e902e106
Benito Lorenzo Santos <benitolsantos@gmail.com>
2018-03-15 16:47:50 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -18,10 +18,6 @@ def countdown_with_sleep(number)
     puts "#{number} SECOND(S)!"
     sleep(1)
     number = number - 1
-    #flipped puts and number order because started counting 1 number too early
-    #originally was
-    #number = number - 1
-    #puts "#{number} SECOND(S)!"
   end
 
   return 'HAPPY NEW YEAR!'
