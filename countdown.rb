ad2772f9c595e04d320861c14fce15c450e40544
Benito Lorenzo Santos <benitolsantos@gmail.com>
2018-03-15 16:44:44 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -2,6 +2,7 @@
 
 def countdown(number)
   while number > 0
+    #flipped puts and number order because started counting 1 number too early
     puts "#{number} SECOND(S)!"
     number = number - 1
   end
